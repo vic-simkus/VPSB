36475ebb4e198050c55c3c51b604c6ac94bc5ace
root <root@beaglebone.localdomain>
2018-12-15 22:15:53 -0600

    Initial import.

---
+++ scripts/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "** Running VPSB startup scriptsi **"
+
+CPIN="/opt/source/bb.org-overlays/tools/beaglebone-universal-io/config-pin"
+
+echo "Setting P9_27 (AUX1) to out and low"
+$CPIN P9_27 low
+
+echo "Setting P8_39 (AUX2) to out and low"
+$CPIN P8_39 low
+
+
+echo "Setting P8_40 (ENABLE) to out and low"
+$CPIN P8_40 low
+
+echo "** VPSB startup script finished **"
+
+exit 0 
